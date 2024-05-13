5aa1b561531b125b641cd7af39b2c96ebc2d5e54
ehsan <ehsan@raychat.io>
2024-05-13 23:43:27 +0330

    implement user schema

---
+++ schemas/user.schema.ts
@@ -0,0 +1,22 @@
+
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type UserDocument = HydratedDocument<User>;
+
+@Schema()
+export class User {
+  @Prop({ required: true, max: 200, min: 1, })
+  email: string;
+
+  @Prop({ required: true, max: 200, min: 1, })
+  password: string;
+
+  @Prop({ required: true, default: Date.now })
+  createdAt: Date;
+
+  @Prop({ required: true, default: Date.now })
+  updatedAt: Date;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
