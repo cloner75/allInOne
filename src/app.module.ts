46f42bedf1fc870b4b1051e3042b8c2d51a1974f
ehsan <ehsan@raychat.io>
2024-05-13 23:44:12 +0330

    usage of configs & mogoose in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,30 @@ import { AppService } from './app.service';
 import { TodoModule } from './todo/todo.module';
 import { ShorterModule } from './shorter/shorter.module';
 import { TicketingModule } from './ticketing/ticketing.module';
-
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import mongodbConfig from 'configs/mongodb.config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from 'schemas/user.schema';
 @Module({
-  imports: [TodoModule, ShorterModule, TicketingModule],
+  imports: [
+    TodoModule,
+    ShorterModule,
+    TicketingModule,
+    AuthModule,
+    ConfigModule.forRoot({
+      envFilePath: ['.env.development', '.env.test', '.env.production'],
+      load: [mongodbConfig]
+    }),
+    MongooseModule.forRoot(process.env.MONGO_URL),
+    MongooseModule.forFeature([
+      {
+        name: User.name,
+        schema: UserSchema
+      }
+    ])
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
