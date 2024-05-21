f235b0bc6a4157bf46f668112efc029a01411083
ehsan <ehsan@raychat.io>
2024-05-21 22:35:58 +0330

    implement short.module

--- src/shorter/shorter.module.ts
+++ src/shorter/shorter.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
-import { ShorterService } from './shorter.service';
-import { ShorterController } from './shorter.controller';
+import { ShorterService } from './services/shorter.service';
+import { ShorterController } from './controllers/shorter.controller';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Link, LinkSchema } from 'schemas/link.schema';
 
 @Module({
+  imports: [
+    ConfigModule,
+    MongooseModule.forFeatureAsync([
+      {
+        name: Link.name,
+        useFactory: () => {
+          const schema = LinkSchema;
+          schema.pre('save', function () {
+            console.log('Hello from pre save');
+          });
+          return schema;
+        }
+      }
+    ]),
+  ],
   controllers: [ShorterController],
   providers: [ShorterService],
 })
-export class ShorterModule {}
+export class ShorterModule { }
